933fbe3e72491f11580a611d5fa79d513976a228
Henry Augustiano <henry.augustiano04@gmail.com>
2023-09-19 17:21:34 -0700

    setup landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to Cloud Inventory</h1>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
