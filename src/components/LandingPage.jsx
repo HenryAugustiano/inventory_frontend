8c4a985d468242d0e967ec19fb224191b8367750
Henry Augustiano <henry.augustiano04@gmail.com>
2023-09-20 14:23:02 -0700

    fix bg color

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -35,7 +35,11 @@ function LandingPage() {
 
       <div 
       className="flex justify-center items-center h-screen"
-      style={{ backgroundImage: `url(${cloudBg})` }}
+      style={{ 
+        backgroundImage: `url(${cloudBg})`, 
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
       >
         <div className="bg-white p-8 rounded-lg shadow-md flex flex-col items-center text-center">
           <h1 className="text-3xl font-bold mb-4">
@@ -172,6 +176,8 @@ return (
 
                   //reset error
                   setStatusMessage("");
+                  setEmailError("");
+                  setPasswordError("");
 
                   setShowModal(false);
                 }}
@@ -212,12 +218,35 @@ return (
                       }}
                     />
                   </div>
+                  <div style={{ width: "500px" }}>
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      password
+                      {passwordError && (
+                        <span style={{ color: "red" }}>
+                          {" "}
+                          ({passwordError})
+                        </span>
+                      )}
+                    </label>
+                    <input
+                      type="password"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                        if (e.target.value !== "") setPasswordError("");
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
 
               <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                 <button
-                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  className="bg-red-600 text-white font-bold uppercase px-6 py-2 text-sm rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                   type="button"
                   onClick={() => {
                     setShowModal(false);
@@ -227,13 +256,16 @@ return (
 
                     //reset error
                     setStatusMessage("");
+                    setEmailError("");
+                    setPasswordError("");
                   }}
                 >
                   Close
                 </button>
                 <button
-                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="submit"
+                 className="bg-emerald-600  text-white font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                 type="button"
+                  onClick={handleSubmit}
                 >
                   Sign In
                 </button>
