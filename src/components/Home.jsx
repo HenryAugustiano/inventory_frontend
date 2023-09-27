39880e658adf90462668119c107a00f1b0946c82
Henry Augustiano <henry.augustiano04@gmail.com>
2023-09-26 18:53:11 -0700

    Mock Home, display user info through API call

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,27 @@ const Home = () => {
   const cookies = new Cookies();
   const token = cookies.get("token");
 
+  //call the api
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_API_URL}/users/info`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      setData(result.data);
+    };
+    fetchData();
+  }, [token]);
+
   return (
     <div>
       <h1>Home</h1>
       <p>Home page content</p>
       <p>Your token is: {token}</p>
+      your data is {JSON.stringify(data)}
     </div>
   );
 };
