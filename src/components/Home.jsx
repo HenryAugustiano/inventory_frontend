39b86cc3d90252c4e6c4ab2a7c2fb452de122f71
Henry Augustiano <henry.augustiano04@gmail.com>
2023-09-27 20:44:28 -0700

    navbar at Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
+
+//Bootstrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 const Home = () => {
 
   const cookies = new Cookies();
@@ -22,12 +29,40 @@ const Home = () => {
   }, [token]);
 
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Home page content</p>
-      <p>Your token is: {token}</p>
-      your data is {JSON.stringify(data)}
-    </div>
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+    <Container fluid>
+      <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+        >
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action2">Link</Nav.Link>
+          <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">
+              Something else here
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#" disabled>
+            Link
+          </Nav.Link>
+        </Nav>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">{data.email}</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 };
 
