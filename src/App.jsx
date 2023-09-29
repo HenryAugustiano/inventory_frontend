c7e46714adb10dd49196abfd6e3d4d446e69afac
Henry Augustiano <henry.augustiano04@gmail.com>
2023-09-29 00:18:34 -0700

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Home from './components/Home';
+import Error404 from './components/404';
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home/>} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
   );
